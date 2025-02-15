dabf9be73157fefc1649db22002aa1660c448968
JacopoTrincanato <trincanatoj@gmail.com>
2025-02-15 15:36:45 +0100

    create methods

--- java-oop-shop-base/src/org/lessons/java/shop/Prodotto.java
+++ java-oop-shop-base/src/org/lessons/java/shop/Prodotto.java
@@ -9,11 +9,26 @@ public class Prodotto {
     float iva;
 
     // costruttore
-    Prodotto(int codice, String nome, String descrizione, float prezzo, float iva) {
+    public Prodotto(int codice, String nome, String descrizione, float prezzo, float iva) {
         this.codice = codice;
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
         this.iva = iva;
+    }
+
+    // metodo per il prezzo base
+    public void prezzoBase(float prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    // metodo per il prezzo comprensivo di iva
+    public void prezzoConIva(float prezzo) {
+        this.prezzo = prezzo * (1 + this.iva);
+    }
+
+    // metodo per il nome esteso
+    public void nomeEsteso(String nome) {
+        nome = this.codice + "-" + this.nome;
     }
 }
